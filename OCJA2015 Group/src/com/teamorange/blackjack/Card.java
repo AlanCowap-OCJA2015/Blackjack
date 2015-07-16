08c391fc2192b3b9660baecdc8129a49f4c408ae
Kevin Phair <kevin.phair@gmail.com>
2015-07-16 13:59:26 +0100

    Card class added

---
+++ OCJA2015 Group/src/com/teamorange/blackjack/Card.java
@@ -0,0 +1,29 @@
+/**
+ * Created 16 Jul 2015
+ * @author Kevin Phair
+ * 
+ */
+package com.teamorange.blackjack;
+
+/**
+ * 
+ * @author Mubasher Mohammed, Kevin Phair, Jenn Quinn, Seamus O'Toole
+ * @version 1.0
+ */
+public class Card {
+
+	enum Suit { DIAMONDS, HEARTS, CLUBS, SPADES }
+	enum Owner { DECK, PLAYER1, PLAYER2 }
+	int faceValue;		// 1 -13
+	
+	public int getScore() {
+		
+		if (this.faceValue > 10) {
+			return 10;
+		} else if (this.faceValue == 1){
+			return 11;
+		} else {
+			return this.faceValue;
+		}
+	}
+}
