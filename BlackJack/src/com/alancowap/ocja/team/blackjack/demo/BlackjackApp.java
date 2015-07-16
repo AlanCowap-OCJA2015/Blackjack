cdca05b2c2b70d709cf653ba89bbc21cd4078349
Alan Cowap <spam@alancowap.com>
2015-07-16 13:12:53 +0100

    Initial commit

---
+++ BlackJack/src/com/alancowap/ocja/team/blackjack/demo/BlackjackApp.java
@@ -0,0 +1,20 @@
+/** 
+ *  Sample app for github repo
+ *  
+ *  
+ *  
+ * @author Alan Cowap 
+ * @version 1.0  
+ * @dependencies None
+ *  
+ */
+package com.alancowap.ocja.team.blackjack.demo;
+
+public class BlackjackApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello github");
+
+	}
+
+}
